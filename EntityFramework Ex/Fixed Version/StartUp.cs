1a886846daf3571d4aab3a4bda6c973e199d894c
StefanYosifov <stefyosifov@gmail.com>
2023-02-24 14:26:04 +0200

    Fixing a bug with invalid object name

--- EntityFramework Ex/Fixed Version/StartUp.cs
+++ EntityFramework Ex/Fixed Version/StartUp.cs
@@ -26,8 +26,12 @@
             Console.WriteLine('\n');
 
 
+            string seventhProblem = GetEmployeesInPeriod2(context);
+            Console.WriteLine(seventhProblem);
+            Console.WriteLine('\n');
 
 
+           
 
         }
 
@@ -123,5 +127,86 @@
 
             return output.ToString().TrimEnd();
         }
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            DateTime minDate = new DateTime(2001, 1, 1);
+            DateTime maxDate = new DateTime(2003, 12, 31);
+
+
+            var employees = context.Employees.
+                Where(e => e.EmployeesProjects.Any
+                (ep => ep.Project.StartDate.Year >= minDate.Year && ep.Project.StartDate.Year <= minDate.Year))
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    ManagerFirstName = e.Manager.FirstName,
+                    ManagerLastName = e.Manager.LastName,
+                    Projects = e.EmployeesProjects
+                    .Select(ep => new
+                    {
+                        ProjectName = ep.Project.Name,
+                        StartDate = ep.Project.StartDate.ToString("M/d/yyyy h:mm:ss tt"),
+                        EndDate = ep.Project.EndDate.HasValue ?
+                        ep.Project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt") : "not finished"
+                    }).ToList()
+                }).Take(10)
+                .ToList();
+
+
+            foreach (var employee in employees)
+            {
+                output.AppendLine($"{employee.FirstName} {employee.LastName} - Manager: {employee.FirstName} {employee.LastName}");
+
+                foreach (var project in employee.Projects)
+                {
+                    var startDate = project.StartDate;
+                    var endDate=project.EndDate;
+
+                    output.AppendLine($"--{project.ProjectName} - {startDate} - {endDate}");
+                }
+            }
+
+
+            return output.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesInPeriod2(SoftUniContext context)
+        {
+            var employees = context.Employees.Where(e => e.EmployeesProjects.Any(ep => ep.Project.StartDate.Year >= 2001 && ep.Project.StartDate.Year <= 2003))
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    ManagerFirstName = e.Manager.FirstName,
+                    ManagerLastName = e.Manager.LastName,
+                    Projects = e.EmployeesProjects.Select(ep => new
+                    {
+                        ProjectName = ep.Project.Name,
+                        ProjectStartDate = ep.Project.StartDate,
+                        ProjectEndDate = ep.Project.EndDate
+                    })
+                }).Take(10);
+
+            StringBuilder employeeManagerResult = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                employeeManagerResult.AppendLine($"{employee.FirstName} {employee.LastName} - Manager: {employee.ManagerFirstName} {employee.ManagerLastName}");
+
+                foreach (var project in employee.Projects)
+                {
+                    var startDate = project.ProjectStartDate.ToString("M/d/yyyy h:mm:ss tt");
+                    var endDate = project.ProjectEndDate.HasValue ? project.ProjectEndDate.Value.ToString("M/d/yyyy h:mm:ss tt") : "not finished";
+
+                    employeeManagerResult.AppendLine($"--{project.ProjectName} - {startDate} - {endDate}");
+                }
+            }
+            return employeeManagerResult.ToString().TrimEnd();
+        }
     }
 }
+    
