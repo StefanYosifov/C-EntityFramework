6da34983651717b49f9e84208f124b3d987e6755
StefanYosifov <stefyosifov@gmail.com>
2023-04-26 18:58:19 +0300

    Commit old exam

--- C# DB Advanced Retake Exam - 15 August 2022/Trucks/DataProcessor/Serializer.cs
+++ C# DB Advanced Retake Exam - 15 August 2022/Trucks/DataProcessor/Serializer.cs
@@ -2,12 +2,32 @@
 {
     using Data;
     using Newtonsoft.Json;
+    using Trucks.DataProcessor.ExportDto;
 
     public class Serializer
     {
         public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
         {
-            throw new NotImplementedException();
+            ExportDespatcherDto[] despatchers = context.Despatchers
+                .Where(d => d.Trucks.Any())
+                .Select(d => new ExportDespatcherDto()
+                {
+                    DespatcherName = d.Name,
+                    TrucksCount = d.Trucks.Count,
+                    Trucks = d.Trucks
+                    .Select(t => new ExportTruckDto()
+                    {
+                        RegistrationNumber = t.RegistrationNumber,
+                        Make = t.MakeType.ToString(),
+                    })
+                    .OrderBy(t => t.RegistrationNumber)
+                    .ToArray()
+                })
+                .OrderByDescending(d => d.TrucksCount)
+                .ThenBy(d => d.DespatcherName)
+                .ToArray();
+
+            return "";
         }
 
         public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
